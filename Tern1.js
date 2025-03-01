4982c13825583211675d53ec38764e94c60cbc4a
Yol1k <artemtimov@mail.ru>
2025-03-01 15:22:06 +0300

    Задание. Конвертация условий.

--- Tern1.js
+++ Tern1.js
@@ -7,18 +7,65 @@
 let a = Math.floor(Math.random() * 100);
 let result;
 
-if ((a > 10 ? a : a * 2) > 5) {
-    result = (2 * a) + 1;
-} else {
-    if ((a < 3 ? 1 : 2 * (a - 2)) > 4) {
-        result = 5;
+if (a > 10) {
+    if (a * 2 > 5) {
+      result = 2 * a + 1;
     } else {
-        if (a % 2 == 0) {
-            result = 6;
+      result = 5;
+    }
+  } else {
+    if (a < 3) {
+      result = 2 * (a - 2);
+    } else {
+      if (a > 4) {
+        result = 5;
+      } else {
+        if (a % 2 === 0) {
+          result = 6;
         } else {
-            result = 7;
+          result = 7;
         }
+      }
+    }
+  }
+
+if (a > 10) {
+    switch (true) {
+        case (a * 2 > 5):
+            result = 2 * a + 1;
+            break;
+        default:
+            result = 5;
+            break;
+    }
+} else {
+    switch (true) {
+        case (a < 3):
+            result = 2 * (a - 2);
+            break;
+        case (a > 4):
+            result = 5;
+            break;
+        default:
+            switch (a % 2) {
+                case 0:
+                    result = 6;
+                    break;
+                default:
+                    result = 7;
+                    break;
+            }
+            break;
     }
 }
+console.log(`Тернаный оператор
+a: ${a} 
+Результат: ${(a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7)}
 
-console.log(result);
+if...else
+a: ${a} 
+Результат: ${result}
+
+switch()
+a: ${a} 
+Результат: ${result}`);
--- Tern2.js
+++ Tern2.js
@@ -1,14 +1,94 @@
+// function manyChecks() {
+//   let a = Math.floor(Math.random() * 20) + 1;
+//   console.log(`a = ${a}`);
+  
+//   return (
+//     a > 10 ? 'a is bigger than 10' : 'a is less than or equal to 10 ' + (a === 5 ? 'an example of a special case' : '')) 
+//     + (a === 15 ? 'but a is not 15' : '')+ (a > 5 ? 'and a is greater than 5' : 'and a is less than or equal to 5 ') 
+//     + (a % 2 ? ' and a is odd' : ' and a is even ');
+// }
+
+// manyChecks();
+// условие с условным (тернарным) оператором перевести в if...else И switch()
+// результат выводить в консоль, с пощью console.log()
+
 function manyChecks() {
   let a = Math.floor(Math.random() * 20) + 1;
-  console.log(`a = ${a}`);
-  
-  return (
-    a > 10 ? 'a is bigger than 10' : 'a is less than or equal to 10 ' + (a === 5 ? 'an example of a special case' : '')) 
-    + (a === 15 ? 'but a is not 15' : '')+ (a > 5 ? 'and a is greater than 5' : 'and a is less than or equal to 5 ') 
-    + (a % 2 ? ' and a is odd' : ' and a is even ');
+  console.log(`a = ${a}
+    `);
+
+  let result = "";
+
+  if (a > 10) {
+    result += "a is bigger than 10 ";
+  } else {
+    result += "a is less than or equal to 10 ";
+    if (a === 5) {
+      result += "an example of a special case";
+    }
+  }
+
+  if (a === 15) {
+    result += "but a is not 15 ";
+  }
+
+  if (a > 5) {
+    result += "and a is greater than 5 ";
+  } else {
+    result += "and a is less than or equal to 5 ";
+  }
+
+  if (a % 2) {
+    result += "and a is odd";
+  } else {
+    result += "and a is even";
+  }
+
+  console.log(`if..else
+Результат: ${result}
+`);
+
+    let resultSwitch = "";
+
+    if (a > 10) {
+        resultSwitch += "a is bigger than 10 ";
+    } else {
+        resultSwitch += "a is less than or equal to 10 ";
+        switch (a) {
+            case 5:
+                resultSwitch += "an example of a special case";
+                break;
+            default:
+                break;
+        }
+    }
+
+    switch (a) {
+        case 15:
+            resultSwitch += "but a is not 15 ";
+            break;
+        default:
+            break;
+    }
+
+    if (a > 5) {
+        resultSwitch += "and a is greater than 5 ";
+    } else {
+        resultSwitch += "and a is less than or equal to 5 ";
+    }
+
+
+    switch (a % 2) {
+        case 0:
+            resultSwitch += "and a is even";
+            break;
+        default:
+            resultSwitch += "and a is odd";
+            break;
+    }
+    console.log(`Switch
+Результат: ${resultSwitch}`);
+
 }
 
 manyChecks();
-
-// условие с условным (тернарным) оператором перевести в if...else И switch()
-// результат выводить в консоль, с пощью console.log()
