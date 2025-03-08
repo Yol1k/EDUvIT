a74828863bb277fadb681a4d64fef15fa766def7
Yol1k <artemtimov@mail.ru>
2025-03-09 02:24:04 +0300

    Исправления
    Точку можно ставить в одном числе только один раз, точку нельзя ставить после символа операции, если не введена цифра

--- CalculatorProject/script.js
+++ CalculatorProject/script.js
@@ -19,6 +19,30 @@ function calculate() {
     }
   }
 
+  function appendToDisplay(value) {
+    const currentValue = display.value;
+    const lastChar = currentValue[currentValue.length - 1];
+
+    const isLastCharOperator = ['+', '-', '*', '/'].includes(lastChar);
+
+    const isNewCharOperator = ['+', '-', '*', '/'].includes(value);
+
+    const isNewCharDot = value === '.';
+
+
+    if (isNewCharDot) {
+        const lastNumber = currentValue.split(/[\+\-\*\/]/).pop();
+        if (lastNumber.includes('.') || isLastCharOperator) {
+            return;
+        }
+    }
+    if (isNewCharOperator && isLastCharOperator) {
+        display.value = currentValue.slice(0, -1) + value;
+    } else {
+        display.value += value;
+    }
+}
+
 function addToHistory(result) {
     const historyEntry = document.createElement('div');
     historyEntry.textContent = display.value + " = " + result;
