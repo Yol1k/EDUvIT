be430eda6fb436fe5c453aa2fd6140239d14042a
Yol1k <artemtimov@mail.ru>
2025-05-04 00:16:43 +0300

    Созданы модели, миграции и шаблоны

--- Django/django_venv/Scripts/activate.bat
+++ Django/django_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\edtim\OneDrive\Рабочий стол\Django\django_venv
+set VIRTUAL_ENV=C:\Users\edtim\OneDrive\Django\django_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
